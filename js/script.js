59a0acdfb9246d72e939dfbbb5c5f602ee1c21f0
Niko Köbler <niko@n-k.de>
2019-12-05 07:43:59 +0100

    working on the timezone settings for icals

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
+---
+layout: null
+---
 function ical(summary, description, location, start, end, url, uid) {
-  var calendar = "BEGIN:VCALENDAR\nVERSION:2.0\nMETHOD:PUBLISH\nPRODID:-//JUG DA//DE\nBEGIN:VEVENT\nUID:" + uid + "@jug-da.de\nDTSTAMP:" + new Date().toISOString().replace(/[-:.]/g, '') + "\nDTSTART:" + start + "\nDTEND:" + end + "\nSUMMARY:JUG DA: " + summary + "\nLOCATION:" + location.replace(/\,/g, '\\,') + "\nDESCRIPTION:Java User Group Darmstadt\\n\\n" + summary + "\\n\\n" + description + "\nURL:" + url + "\nSTATUS:CONFIRMED\nCLASS:PUBLIC\nORGANIZER;CN=\"JUG Darmstadt\":MAILTO:no-reply@jug-da.de\nEND:VEVENT\nEND:VCALENDAR";
+  var calendar = "BEGIN:VCALENDAR\n" +
+  "VERSION:2.0\n" +
+  "METHOD:PUBLISH\n" +
+  "CALSCALE:GREGORIAN\n" +
+  "PRODID:-//JUG DA//Veranstaltungskalender//DE\n" +
+  "BEGIN:VEVENT\n" +
+  "UID:" + uid + "@jug-da.de\n" +
+  "DTSTAMP:" + new Date().toISOString().replace(/[-:.]/g, '') + "\n" +
+  "DTSTART;TZID={{ site.timezone }}:" + start + "\n" +
+  "DTEND;TZID={{ site.timezone }}:" + end + "\n" +
+  "SUMMARY:JUG DA: " + summary + "\n" +
+  "LOCATION:" + location.replace(/\,/g, '\\,') + "\n" +
+  "DESCRIPTION:{{ site.name }}\\n\\n" + summary + "\\n\\n" + description + "\n" +
+  "URL:" + url + "\n" +
+  "STATUS:CONFIRMED\n" +
+  "CLASS:PUBLIC\n" +
+  "ORGANIZER;CN=\"JUG Darmstadt\":MAILTO:no-reply@jug-da.de\n" +
+  "END:VEVENT\n" +
+  "END:VCALENDAR";
   var link = document.createElement('a');
   link.download = "jugda_" + uid + ".ics";
-  link.href = "data:text/calendar;charset=utf-8," + escape(calendar);
+  link.href = "data:text/calendar;charset=utf-8," + calendar;
   link.click();
 }
