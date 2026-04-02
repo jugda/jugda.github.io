e6475185fc91c8e0ee136f9d27d23cbf1dc53893
dasniko <dasniko@users.noreply.github.com>
2026-04-02 20:30:10 +0000

    deploy: c483062c7d96706be4fcefc684c4297fc2859010

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const nl = "\r\n";
+const tz = "BEGIN:VTIMEZONE" + nl +
+    "TZID:Europe/Berlin" + nl +
+    "X-LIC-LOCATION:Europe/Berlin" + nl +
+    "BEGIN:DAYLIGHT" + nl +
+    "TZOFFSETFROM:+0100" + nl +
+    "TZOFFSETTO:+0200" + nl +
+    "TZNAME:CES" + nl +
+    "DTSTART:19700329T020000" + nl +
+    "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3" + nl +
+    "END:DAYLIGHT" + nl +
+    "BEGIN:STANDARD" + nl +
+    "TZOFFSETFROM:+0200" + nl +
+    "TZOFFSETTO:+0100" + nl +
+    "TZNAME:CET" + nl +
+    "DTSTART:19701025T030000" + nl +
+    "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10" + nl +
+    "END:STANDARD" + nl +
+    "END:VTIMEZONE" + nl;
+
+function currentDateTime() {
+    var dateNow = new Date().toISOString().replace(/[-:.]/g, '')
+    return dateNow.substr(0,15);
+}
+
+function ical(summary, description, location, start, end, url, uid) {
+  var calendar = "BEGIN:VCALENDAR" + nl +
+  "VERSION:2.0" + nl +
+  "METHOD:PUBLISH" + nl +
+  tz +
+  "CALSCALE:GREGORIAN" + nl +
+  "PRODID:-//JUG DA//Veranstaltungskalender//DE" + nl +
+  "BEGIN:VEVENT" + nl +
+  "UID:" + uid + "@jug-da.de" + nl +
+  "DTSTAMP:" + currentDateTime() + nl +
+  "DTSTART;TZID=Europe/Berlin:" + start + nl +
+  "DTEND;TZID=Europe/Berlin:" + end + nl +
+  "SUMMARY:JUG DA: " + summary + nl +
+  "LOCATION:" + location.replace(/\,/g, '\\,') + nl +
+  "DESCRIPTION:Java User Group Darmstadt\\n\\n" + summary + "\\n\\n" + description + nl +
+  "URL:" + url + nl +
+  "STATUS:CONFIRMED" + nl +
+  "CLASS:PUBLIC" + nl +
+  "ORGANIZER;CN=\"JUG Darmstadt\":MAILTO:no-reply@jug-da.de" + nl +
+  "END:VEVENT" + nl +
+  "END:VCALENDAR";
+  var link = document.createElement('a');
+  link.download = "jugda_" + uid + ".ics";
+  link.href = "data:text/calendar;charset=utf-8," + calendar;
+  link.click();
+}
